2705293d41840fb37ffd0697c491feeb2450ead3
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2019-08-02 15:09:12 +0700

    Add voting stage

---
+++ src/Day.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './styles/style.css';
+import DayLogic from './DayLogic';
+
+class Day extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    // Insert day counter handler here..
+
+    render() {
+        return (
+            <div className="base">
+                <h1>Day x</h1> {/* x supposed to be changed with counter from the server */}
+                <DayLogic />
+            </div>
+        )
+    }
+}
+
+export default Day;
---
+++ src/DayLogic.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import './styles/style.css';
+import PlayerList from './PlayerList';
+
+class DayLogic extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {stage: "", pick: ""}
+        this.handleVoteStage = this.handleVoteStage.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleAllVoted = this.handleAllVoted.bind(this);
+    }
+
+    // Insert broadcast handler for temporary vote here
+
+    handleVoteStage(e) {
+        this.setState({stage: "vote"});
+    }
+
+    handleChange(e) {
+        this.setState({pick: e});
+    }
+
+    handleSubmit(e) {
+        // Insert submit selection to server here
+        this.setState({stage: "voted"});
+    }
+
+    handleAllVoted(e) {
+        // render day summary
+    }
+
+    render() {
+        if (this.state.stage === "vote") {
+            return (
+                <div>
+                    {/* Could add timer probably? */}
+                    <p>Vote who should be hanged:</p>
+                    <PlayerList radio={true} handleChange={(e) => this.handleChange(e)}/>
+                    <button onClick={this.handleSubmit}>Vote</button>
+                    {/* Insert temporary vote results here */}
+                </div>
+            )
+        } else if (this.state.stage === "voted") {
+            return (
+                <div>
+                    {/* Could add timer probably? Or continue after all players voted*/}
+                    <p>You have voted {this.state.pick} to be hanged.</p>
+                    {/* Insert temporary vote results here */}
+                    <button onClick={this.handleAllVoted}>Test AllVoted</button> {/* Test */}
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <p>Discuss with all settlers who should be hanged.</p>
+                    {/* Could add timer probably? */}
+                    <button onClick={this.handleVoteStage}>Test Vote</button>
+                </div>
+            )
+        }
+    }
+}
+
+export default DayLogic;
