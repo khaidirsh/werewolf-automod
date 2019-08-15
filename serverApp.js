c45a329f67f49ef97c20ae8ad89a7d1359ac26a9
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2019-08-15 22:32:03 +0700

    Add player list file init. Use addPlayer.js Add disconnect message.

--- serverApp.js
+++ serverApp.js
@@ -3,12 +3,15 @@ const path = require('path');
 const app = express()
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const fs = require('fs');
 const port = 9000
 
-/*
+const addPlayer = require('./serverfiles/addPlayer');
+
+/* FEATURE TO BE ADDED: CONTINUE IF PLAYER DISCONNECTED
 app.use(express.cookieParser());
 
-// set a cookie to continue game (TO BE BUILT)
+// set a cookie
 app.use(function (req, res, next) {
   // check if client sent cookie
   var cookie = req.cookies.cookieName;
@@ -29,17 +32,27 @@ app.use(function (req, res, next) {
 });
 */
 
+// Initialize files
+fs.writeFile('./serverfiles/playerList.json', JSON.stringify([]), (err) => {
+    if (err) throw err;
+    console.log("playerList initialized!")
+})
+
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-io.on('connection', async (socket) => {
+io.on('connection', (socket) => {
     console.log('User connected with ID: ' + socket.id);
-    socket.on('nick', function(nick) {
-        console.log(nick);
-    })
+    socket.on("nick", (nick) => {
+        addPlayer(socket.id, nick)
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`User with ID: ${socket.id} disconnected`);
+    });
 });
 
-server.listen(port, () => console.log(`Server listening on port ${port}!`))
+server.listen(port, () => console.log(`Server listening on port ${port}!`));
