05b35dd6048cf3a81ed869b696031ce9d9924357
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2019-08-16 18:44:10 +0700

    Add admin status listener

--- src/App.js
+++ src/App.js
@@ -5,39 +5,52 @@ import './styles/style.css';
 import socket from './api/socketConnect';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state={nick: ""};
+    constructor(props) {
+        super(props);
+        this.state = { nick: "", admin: false };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({nick: e.target.value})
-  }
-
-  handleSubmit(e) {
-    if (this.state.nick) {
-      socket.emit("nick", this.state.nick);
-      ReactDOM.render(<Lobby nick={this.state.nick} />, document.getElementById('root'));
-      e.preventDefault();
-    } else {
-      alert("You have to enter your nickname.");
-      e.preventDefault();
+        this.handleChange = this.handleChange.bind(this);
+        this.handleAdmin = this.handleAdmin.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-  }
 
-  render() {
-    return (
-      <div className="base">
-        <h1>Werewolf Automod</h1>
-        <p>Enter your nickname</p>
-        <input id="nick" onChange={this.handleChange}/>
-        <button onClick={this.handleSubmit}>Enter</button>
-      </div>
-    );
-  }
+    handleChange(e) {
+        this.setState({ nick: e.target.value })
+    }
+
+    handleAdmin(e) {
+        this.setState({ admin: e });
+        console.log(`Your admin status: ${this.state.admin}`);
+        ReactDOM.render(<Lobby nick={this.state.nick} admin={this.state.admin} />, document.getElementById('root'));
+    }
+
+    handleSubmit(e) {
+        if (this.state.nick) {
+            // send nick to server
+            socket.emit("nick", this.state.nick);
+
+            e.preventDefault();
+        } else {
+            alert("You have to enter your nickname.");
+            e.preventDefault();
+        }
+    }
+
+    componentDidMount() {
+        // retreive admin previleges from server
+        socket.on('admin', this.handleAdmin)
+    }
+
+    render() {
+        return (
+            <div className="base">
+                <h1>Werewolf Automod</h1>
+                <p>Enter your nickname</p>
+                <input id="nick" onChange={this.handleChange} />
+                <button onClick={this.handleSubmit}>Enter</button>
+            </div>
+        );
+    }
 }
 
 export default App;
