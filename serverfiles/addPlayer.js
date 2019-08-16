d1ce15ba6857f10294c71fd94e4183b04db31cc8
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2019-08-16 18:45:34 +0700

    Add admin status emitter

--- serverfiles/addPlayer.js
+++ serverfiles/addPlayer.js
@@ -1,14 +1,24 @@
 const fs = require('fs');
 
-module.exports = function (socketId, nickname) {
-    let obj = {id: socketId, nick: nickname};
+module.exports = function (socketId, nickname, io) {
+    let obj = { id: socketId, nick: nickname, admin: false };
     fs.readFile('./serverfiles/playerList.json', (err, data) => {
         if (err) throw err;
         let playerList = JSON.parse(data);
+
+        // Set admin to first connected player
+        if (playerList.length === 0) {
+            obj.admin = true;
+        }
+
         playerList.push(obj);
         fs.writeFile('./serverfiles/playerList.json', JSON.stringify(playerList), (err) => {
             if (err) throw err;
             console.log(`${nickname} added to playerList!`)
         })
+
+        // Send admin status to client
+        io.to(socketId).emit('admin', obj.admin);
+        console.log(`${socketId} admin: (${obj.admin})`)
     });
 }
