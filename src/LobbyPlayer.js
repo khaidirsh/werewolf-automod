e0ff7e55d24f9d970b422be6eaed12a6bbf4627f
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2019-08-19 16:23:17 +0700

    Add socket listener to start the game

--- src/LobbyPlayer.js
+++ src/LobbyPlayer.js
@@ -4,16 +4,24 @@ import './styles/style.css';
 import ViewRole from './ViewRole';
 import PlayerList from './PlayerList';
 
+import socket from './api/socketConnect';
+
 class LobbyMaster extends React.Component {
     constructor(props) {
         super(props);
         this.state = {status: "wait", playerRole: ""};
-        this.handleStatus = this.handleStatus.bind(this);
     }
 
-    handleStatus(e) {
-        // If server sends status to start the game:
-        this.setState({status: "start", playerRole: e.target.value /* Change to playerRole received from server */});
+    componentDidMount() {
+        // Add socket listener for game status
+        socket.on('player role', (roleFromServer) => {
+            this.setState({status: "start", playerRole: roleFromServer});
+        })
+    }
+
+    componentWillUnmount() {
+        // Remove socket listener
+        socket.off('player role');
     }
 
     render() {
@@ -24,7 +32,7 @@ class LobbyMaster extends React.Component {
             title =  "Starting game...";
             setTimeout(() => {
                 ReactDOM.render(<ViewRole playerRole={this.state.playerRole}/>, document.getElementById("root"))
-            }, 1000)
+            }, 3000)
         }
         return (
             <div className="base">
