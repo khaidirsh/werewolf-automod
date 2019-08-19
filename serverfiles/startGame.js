4d3fd1c71314a4c6d0ad8d1fe7eec89e256cad46
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2019-08-19 17:27:24 +0700

    Move server generated files. Add main role and player ready state index

--- serverfiles/startGame.js
+++ serverfiles/startGame.js
@@ -5,7 +5,7 @@ module.exports = function (socketId, io) {
     console.log('Checking player count..')
 
     // read player list
-    fs.readFile('./serverfiles/playerList.json', (err, data) => {
+    fs.readFile('./serverfiles/json/playerList.json', (err, data) => {
         if (err) throw err;
 
         let playerList = JSON.parse(data);
@@ -20,22 +20,42 @@ module.exports = function (socketId, io) {
 
             // Create array of main roles
             let rolesArr = ['werewolf', 'seer', 'guardian'];
-            let mainRoles = rolesArr.length;
+            let mainRolesCount = rolesArr.length;
 
             // Add villagers for remaining role
-            for (let i = 0; i < (playerCount - mainRoles); i++) {
+            for (let i = 0; i < (playerCount - mainRolesCount); i++) {
                 rolesArr.push('villager');
             }
 
             // Shuffle array
             shuffledRoles = shuffle(rolesArr);
 
-            // Send role to respective player
+            // Create index for main roles and ready state
+            let mainRoles = {};
+            let playerReady = {};
+
             for (let i = 0; i < playerCount; i++) {
+                // Fill main roles index
+                if (shuffledRoles[i] !== 'villager') {
+                    mainRoles[shuffledRoles[i]] = playerList[i].id;
+                }
+
+                // Send role to respective player
                 io.to(playerList[i].id).emit('player role', shuffledRoles[i]);
                 console.log(`Role sent to ${playerList[i].nick}`)
+
+                // Fill player ready state index
+                playerReady[playerList[i].id] = false;
             }
-            console.log('Waiting for all players to be ready..')
+            // Save indexes to file
+            fs.writeFile('./serverfiles/json/mainRoles.json', JSON.stringify(mainRoles), (err) => {
+                if (err) throw err;
+                console.log(`Main roles indexed`)
+            })
+            fs.writeFile('./serverfiles/json/playerReady.json', JSON.stringify(playerReady), (err) => {
+                if (err) throw err;
+                console.log(`Player state indexed`)
+            })
         }
     });
 }
