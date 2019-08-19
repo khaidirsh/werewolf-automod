73868ce3b4a97f7fbbe91f8e93330b0f7bd48def
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2019-08-19 16:22:53 +0700

    Disable testing buttons Add socket emitter & listener to start game

--- src/LobbyMaster.js
+++ src/LobbyMaster.js
@@ -18,9 +18,26 @@ class LobbyMaster extends React.Component {
         socket.emit("start game")
     }
 
-    handleStatus(e) {
-        // If server sends status to start the game:
-        this.setState({status: "start", playerRole: e.target.value /* Change to playerRole received from server */});
+    /* handleStatus(e) {
+        // For testing buttons:
+        this.setState({status: "start", playerRole: e.target.value});
+    } */
+
+    componentDidMount() {
+        // Add socket listener for game status
+        socket.on('insufficient', () => {
+            alert('There has to be minimum 5 players to start the game.')
+        })
+
+        socket.on('player role', (roleFromServer) => {
+            this.setState({status: "start", playerRole: roleFromServer});
+        })
+    }
+
+    componentWillUnmount() {
+        // Remove socket listener
+        socket.off('insufficient');
+        socket.off('player role');
     }
 
     render() {
@@ -31,17 +48,20 @@ class LobbyMaster extends React.Component {
             title =  "Starting game...";
             setTimeout(() => {
                 ReactDOM.render(<ViewRole playerRole={this.state.playerRole}/>, document.getElementById("root"))
-            }, 1000)
+            }, 3000)
         }
         return (
             <div className="base">
                 <h1>{title}</h1>
                 <p>Player list:</p>
                 <PlayerList />
-                <button onClick={this.handleStatus} value="werewolf">Test as Werewolf</button> {/* Test */}
-                <button onClick={this.handleStatus} value="seer">Test as Seer</button> {/* Test */}
-                <button onClick={this.handleStatus} value="guardian">Test as Guardian</button> {/* Test */}
-                <button onClick={this.handleStatus} value="villager">Test as Villager</button> {/* Test */}
+
+                 {/* Testing buttons */}
+                {/* <button onClick={this.handleStatus} value="werewolf">Test as Werewolf</button>
+                <button onClick={this.handleStatus} value="seer">Test as Seer</button>
+                <button onClick={this.handleStatus} value="guardian">Test as Guardian</button>
+                <button onClick={this.handleStatus} value="villager">Test as Villager</button> */}
+
                 <button onClick={this.handleStartServer}>Start the game</button>
             </div>
         )
